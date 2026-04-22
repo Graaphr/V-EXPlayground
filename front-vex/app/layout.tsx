d51f0b6ce1c1e08153c42f250b7115a6f9f6cf14
faleft <fajrinurpras@gmail.com>
2026-04-22 10:58:12 +0700

    edit layout

--- front-vex/app/layout.tsx
+++ front-vex/app/layout.tsx
@@ -34,7 +34,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en" suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning data-scroll-behavior="smooth">
       <body className={`${poppins.variable} ${tiltWarp.variable} font-poppins`}>
         {children}
       </body>
