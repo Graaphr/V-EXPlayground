7219b1cccb1156457cd19ac558bb58c5d52ae76b
fajpras <fajrinurpras07@gmail.com>
2026-05-03 11:32:27 +0700

    mengubah dari sactrum ke token

--- front-vex/lib/axios.ts
+++ front-vex/lib/axios.ts
@@ -1,14 +1,23 @@
-import axios from 'axios';
+import axios from "axios";
 
 const api = axios.create({
-    // Sesuaikan dengan alamat Laravel Anda yang sedang jalan
-    baseURL: 'http://localhost:8000', 
-    withCredentials: true, 
-    withXSRFToken: true, 
-    headers: {
-        'X-Requested-With': 'XMLHttpRequest',
-        'Accept': 'application/json',
-    },
+  baseURL: "http://localhost:8000",
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
+});
+
+api.interceptors.request.use((config) => {
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+  }
+
+  return config;
 });
 
 export default api;
